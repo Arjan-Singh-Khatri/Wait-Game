2897b8c31d0a37474e2d4bf56be131f1024a22c1
Prabesh002 <149858321+Prabesh002@users.noreply.github.com>
2024-07-01 12:28:09 +0545

    Enemy Update Final

--- Wait Game/Assets/Nellow/Main/Scripts/Enemy/EnemyAI.cs
+++ Wait Game/Assets/Nellow/Main/Scripts/Enemy/EnemyAI.cs
@@ -4,223 +4,365 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-	public enum State
-	{
-		Patrolling,
-		Alerted,
-		Chasing,
-		Looking
-	}
+    public enum State
+    {
+        Patrolling,
+        Alerted,
+        Chasing,
+        Looking,
+        Stalking,
+        Intimidating
+    }
 
-	public Transform player;
-	public LayerMask obstacleLayer;
-	public float normalSpeed = 2f;
-	public float alertSpeed = 4f;
-	public float chaseSpeed = 6f;
-	public float detectionRadius = 10f;
-	public float patrolRange = 20f;
-	public float playerChaseTime = 5f;
-	public float alertDuration = 3f;
-	public State currentState = State.Patrolling;
-	public EnemyAnimation enemyAnimation;
-	private NavMeshAgent navMeshAgent;
-	private bool playerIsLoud = false;
-	private float chaseTimer = 0f;
-	private float alertTimer = 0f;
-	private Vector3 lastNoisePosition;
-	private Vector3 lastPatrolPosition;
+    public Transform player;
+    public LayerMask obstacleLayer;
+    public float normalSpeed = 2f;
+    public float alertSpeed = 4f;
+    public float chaseSpeed = 6f;
+    public float detectionRadius = 10f;
+    public float patrolRange = 20f;
+    public float playerChaseTime = 5f;
+    public float alertDuration = 3f;
+    public float stalkingDistance = 15f;
+    public float followDetectionTime = 3f; 
+    public State currentState = State.Patrolling;
+    public EnemyAnimation enemyAnimation;
+    private NavMeshAgent navMeshAgent;
+    private bool playerIsLoud = false;
+    private float chaseTimer = 0f;
+    private float alertTimer = 0f;
+    private float followTimer = 0f;
+    private Vector3 lastNoisePosition;
+    private Vector3 lastPatrolPosition;
+    private bool playerSeen = false;
 
-	void Start()
-	{
-		navMeshAgent = GetComponent<NavMeshAgent>();
-		MoveToRandomPosition();
-	}
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        MoveToRandomPosition();
+    }
 
-	void Update()
-	{
-		HandleState();
+    void Update()
+    {
+        HandleState();
 
-		if (currentState == State.Chasing)
-		{
-			chaseTimer += Time.deltaTime;
-			if (chaseTimer > playerChaseTime || Vector3.Distance(transform.position, player.position) > detectionRadius * 1.5f)
-			{
-				TransitionToState(State.Patrolling);
-				chaseTimer = 0f;
-				MoveToRandomPosition();
-			}
-		}
-		else if (Vector3.Distance(transform.position, player.position) < detectionRadius && HasLineOfSight())
-		{
-			TransitionToState(State.Chasing);
-		}
-	}
+        if (currentState == State.Chasing)
+        {
+            chaseTimer += Time.deltaTime;
+            if (chaseTimer > playerChaseTime || Vector3.Distance(transform.position, player.position) > detectionRadius * 1.5f)
+            {
+                TransitionToState(State.Patrolling);
+                chaseTimer = 0f;
+                MoveToRandomPosition();
+            }
+        }
+        else if (currentState != State.Stalking && Vector3.Distance(transform.position, player.position) < detectionRadius && HasLineOfSight())
+        {
+            TransitionToState(State.Chasing);
+        }
 
-	private void HandleState()
-	{
-		switch (currentState)
-		{
-			case State.Patrolling:
-				Patrol();
-				break;
-			case State.Alerted:
-				Alert();
-				break;
-			case State.Chasing:
-				ChasePlayer();
-				break;
-			case State.Looking:
-				enemyAnimation.UpdateAnimationState(0);
-				break;
-		}
-	}
+        if (currentState == State.Stalking)
+        {
+            if (IsPlayerSeeingEnemy())
+            {
+                SprintAndHide();
+            }
+            else if (IsPlayerFollowing())
+            {
+                TransitionToState(State.Intimidating);
+            }
+        }
+    }
 
-	private void TransitionToState(State newState)
-	{
-		if (newState == State.Chasing || currentState != State.Chasing)
-		{
-			currentState = newState;
-		}
-		else if (newState == State.Alerted && currentState != State.Chasing && currentState != State.Alerted)
-		{
-			currentState = newState;
-		}
-		else if (newState == State.Patrolling && currentState == State.Looking)
-		{
-			currentState = newState;
-		}
+    private void HandleState()
+    {
+        switch (currentState)
+        {
+            case State.Patrolling:
+                Patrol();
+                break;
+            case State.Alerted:
+                Alert();
+                break;
+            case State.Chasing:
+                ChasePlayer();
+                break;
+            case State.Looking:
+                enemyAnimation.UpdateAnimationState(0);
+                break;
+            case State.Stalking:
+                StalkPlayer();
+                break;
+            case State.Intimidating:
+                IntimidatePlayer();
+                break;
+        }
+    }
 
-		if (currentState == State.Looking)
-		{
-			StopCoroutine(LookAround());
-			enemyAnimation.PlayLookAroundAnimation(false);
-			navMeshAgent.isStopped = false;
-		}
-	}
+    private void TransitionToState(State newState)
+    {
+        if (newState == State.Chasing || currentState != State.Chasing)
+        {
+            currentState = newState;
+        }
+        else if (newState == State.Alerted && currentState != State.Chasing && currentState != State.Alerted)
+        {
+            currentState = newState;
+        }
+        else if (newState == State.Patrolling && currentState == State.Looking)
+        {
+            currentState = newState;
+        }
+        else if (newState == State.Stalking && currentState != State.Chasing)
+        {
+            currentState = newState;
+        }
 
-	public void SetPlayerLoud(bool isLoud)
-	{
-		playerIsLoud = isLoud;
-		if (isLoud)
-		{
-			lastNoisePosition = player.position;
-			TransitionToState(State.Alerted);
-			alertTimer = 0f;
-		}
-	}
+        if (currentState == State.Looking)
+        {
+            StopCoroutine(LookAround());
+            enemyAnimation.PlayLookAroundAnimation(false);
+            navMeshAgent.isStopped = false;
+        }
+    }
 
-	private void Patrol()
-	{
-		navMeshAgent.speed = normalSpeed;
-		enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
+    public void SetPlayerLoud(bool isLoud)
+    {
+        playerIsLoud = isLoud;
+        if (isLoud)
+        {
+            lastNoisePosition = player.position;
+            TransitionToState(State.Alerted);
+            alertTimer = 0f;
+        }
+    }
 
-		if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
-		{
-			StartCoroutine(LookAround());
-		}
-	}
+    public void SetPlayerQuiet(bool isQuiet)
+    {
+        if (isQuiet && currentState != State.Stalking && currentState != State.Chasing)
+        {
+            TransitionToState(State.Stalking);
+        }
+    }
 
-	private void Alert()
-	{
-		navMeshAgent.speed = alertSpeed;
-		enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
-		navMeshAgent.SetDestination(lastNoisePosition);
+    private void Patrol()
+    {
+        navMeshAgent.speed = normalSpeed;
+        enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
 
-		alertTimer += Time.deltaTime;
-		if (alertTimer >= alertDuration || navMeshAgent.remainingDistance <= 1f)
-		{
-			StartCoroutine(LookAround());
-		}
-	}
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+        {
+            StartCoroutine(LookAround());
+        }
+    }
 
-	private void ChasePlayer()
-	{
-		navMeshAgent.speed = chaseSpeed;
-		navMeshAgent.SetDestination(player.position);
-		enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
-	}
+    private void Alert()
+    {
+        navMeshAgent.speed = alertSpeed;
+        enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
+        navMeshAgent.SetDestination(lastNoisePosition);
 
-	private void MoveToRandomPosition()
-	{
-		Vector3 randomDirection;
-		Vector3 finalPosition;
-		do
-		{
-			randomDirection = Random.insideUnitSphere * patrolRange;
-			randomDirection += transform.position;
-			NavMeshHit hit;
-			NavMesh.SamplePosition(randomDirection, out hit, patrolRange, 1);
-			finalPosition = hit.position;
-		} while (Vector3.Distance(finalPosition, lastPatrolPosition) < patrolRange / 2);
+        alertTimer += Time.deltaTime;
+        if (alertTimer >= alertDuration || navMeshAgent.remainingDistance <= 1f)
+        {
+            StartCoroutine(LookAround());
+        }
+    }
 
-		lastPatrolPosition = finalPosition;
-		navMeshAgent.SetDestination(finalPosition);
-	}
+    private void ChasePlayer()
+    {
+        navMeshAgent.speed = chaseSpeed;
+        navMeshAgent.SetDestination(player.position);
+        enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
+    }
 
-	private IEnumerator LookAround()
-	{
-		currentState = State.Looking;
-		navMeshAgent.isStopped = true;
+    private void MoveToRandomPosition()
+    {
+        Vector3 randomDirection;
+        Vector3 finalPosition;
+        do
+        {
+            randomDirection = Random.insideUnitSphere * patrolRange;
+            randomDirection += transform.position;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, patrolRange, 1);
+            finalPosition = hit.position;
+        } while (Vector3.Distance(finalPosition, lastPatrolPosition) < patrolRange / 2);
 
-		float lookDuration = 3f;
-		float lookTimer = 0f;
-		enemyAnimation.PlayLookAroundAnimation(true);
+        lastPatrolPosition = finalPosition;
+        navMeshAgent.SetDestination(finalPosition);
+    }
 
-		while (lookTimer < lookDuration)
-		{
-			if (currentState != State.Looking)
-			{
-				enemyAnimation.PlayLookAroundAnimation(false);
-				navMeshAgent.isStopped = false;
-				yield break;
-			}
+    private IEnumerator LookAround()
+    {
+        currentState = State.Looking;
+        navMeshAgent.isStopped = true;
 
-			if (Vector3.Distance(transform.position, player.position) < detectionRadius && HasLineOfSight())
-			{
-				TransitionToState(State.Chasing);
-				navMeshAgent.isStopped = false;
-				enemyAnimation.PlayLookAroundAnimation(false);
-				yield break;
-			}
+        float lookDuration = 3f;
+        float lookTimer = 0f;
+        enemyAnimation.PlayLookAroundAnimation(true);
 
-			lookTimer += Time.deltaTime;
-			yield return null;
-		}
+        while (lookTimer < lookDuration)
+        {
+            if (currentState != State.Looking)
+            {
+                enemyAnimation.PlayLookAroundAnimation(false);
+                navMeshAgent.isStopped = false;
+                yield break;
+            }
 
-		enemyAnimation.PlayLookAroundAnimation(false);
-		navMeshAgent.isStopped = false;
-		TransitionToState(State.Patrolling);
-		MoveToRandomPosition();
-	}
+            if (Vector3.Distance(transform.position, player.position) < detectionRadius && HasLineOfSight())
+            {
+                TransitionToState(State.Chasing);
+                navMeshAgent.isStopped = false;
+                enemyAnimation.PlayLookAroundAnimation(false);
+                yield break;
+            }
 
-	private bool HasLineOfSight()
-	{
-		Vector3 directionToPlayer = player.position - transform.position;
-		if (Physics.Linecast(transform.position, player.position, out RaycastHit hit, obstacleLayer))
-		{
-			return hit.transform == player;
-		}
-		return true;
-	}
+            lookTimer += Time.deltaTime;
+            yield return null;
+        }
 
-	private void OnDrawGizmosSelected()
-	{
-		Gizmos.color = Color.red;
-		Gizmos.DrawWireSphere(transform.position, detectionRadius);
+        enemyAnimation.PlayLookAroundAnimation(false);
+        navMeshAgent.isStopped = false;
+        TransitionToState(State.Patrolling);
+        MoveToRandomPosition();
+    }
 
-		Gizmos.color = Color.green;
-		Gizmos.DrawWireSphere(transform.position, patrolRange);
+    private void StalkPlayer()
+    {
+        navMeshAgent.speed = normalSpeed; 
+        Vector3 directionToPlayer = player.position - transform.position;
+        float desiredDistance = stalkingDistance;
+        Vector3 stalkingPosition = player.position - directionToPlayer.normalized * desiredDistance;
 
-		if (currentState == State.Alerted)
-		{
-			Gizmos.color = Color.yellow;
-			Gizmos.DrawWireSphere(lastNoisePosition, 1f);
-		}
+        // Hide behind obstacles if possible
+        if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, desiredDistance, obstacleLayer))
+        {
+            Vector3 hidingSpot = hit.point + hit.normal * 1.5f;
+            if (NavMesh.SamplePosition(hidingSpot, out NavMeshHit navHit, 1.5f, NavMesh.AllAreas))
+            {
+                stalkingPosition = navHit.position;
+            }
+        }
 
-		if (currentState == State.Chasing)
-		{
-			Gizmos.color = Color.blue;
-			Gizmos.DrawLine(transform.position, player.position);
-		}
-	}
+        navMeshAgent.SetDestination(stalkingPosition);
+        enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
+
+        if (Vector3.Distance(transform.position, player.position) < detectionRadius * 0.3f && HasLineOfSight())
+        {
+            TransitionToState(State.Chasing); 
+        }
+    }
+
+    private bool HasLineOfSight()
+    {
+        Vector3 directionToPlayer = player.position - transform.position;
+        if (Physics.Linecast(transform.position, player.position, out RaycastHit hit, obstacleLayer))
+        {
+            return hit.transform == player;
+        }
+        return true;
+    }
+
+    private bool IsPlayerSeeingEnemy()
+    {
+        Vector3 toPlayer = player.position - transform.position;
+        float angle = Vector3.Angle(player.forward, toPlayer);
+        if (angle < 90f && Vector3.Distance(player.position, transform.position) < detectionRadius && HasLineOfSight())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool IsPlayerFollowing()
+    {
+        if (Vector3.Distance(transform.position, player.position) < stalkingDistance && HasLineOfSight())
+        {
+            followTimer += Time.deltaTime;
+            if (followTimer >= followDetectionTime)
+            {
+                followTimer = 0f;
+                return true;
+            }
+        }
+        else
+        {
+            followTimer = 0f;
+        }
+        return false;
+    }
+
+    private void SprintAndHide()
+    {
+        navMeshAgent.speed = chaseSpeed;
+        Vector3 hidingSpot = Vector3.zero;
+        if (Physics.Raycast(transform.position, (player.position - transform.position).normalized, out RaycastHit hit, detectionRadius, obstacleLayer))
+        {
+            hidingSpot = hit.point + hit.normal * 2f;
+            if (NavMesh.SamplePosition(hidingSpot, out NavMeshHit navHit, 2f, NavMesh.AllAreas))
+            {
+                hidingSpot = navHit.position;
+            }
+        }
+
+        if (hidingSpot != Vector3.zero)
+        {
+            navMeshAgent.SetDestination(hidingSpot);
+            currentState = State.Stalking;
+            playerSeen = true;
+        }
+    }
+
+    private void IntimidatePlayer()
+    {
+        navMeshAgent.speed = chaseSpeed;
+        navMeshAgent.SetDestination(player.position);
+        enemyAnimation.UpdateAnimationState(navMeshAgent.velocity.magnitude);
+
+        if (Vector3.Distance(transform.position, player.position) > detectionRadius * 1.5f)
+        {
+            TransitionToState(State.Patrolling);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, patrolRange);
+
+        if (currentState == State.Alerted)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(lastNoisePosition, 1f);
+        }
+
+        if (currentState == State.Chasing)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, player.position);
+        }
+
+        if (currentState == State.Stalking)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawLine(transform.position, navMeshAgent.destination);
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (currentState == State.Stalking)
+        {
+            GUI.Label(new Rect(10, 10, 200, 20), "Enemy is Stalking");
+        }
+        else if (currentState == State.Intimidating)
+        {
+            GUI.Label(new Rect(10, 30, 200, 20), "Enemy is Intimidating");
+        }
+    }
 }
--- Wait Game/Assets/Nellow/Main/Scripts/Player/Input/MicInput.cs
+++ Wait Game/Assets/Nellow/Main/Scripts/Player/Input/MicInput.cs
@@ -4,76 +4,81 @@ using UnityEngine.UI;
 
 public class MicInput : MonoBehaviour
 {
-    public float volumeThreshold = 0.1f;
-    public float sliderAmplifier = 1000f;
-    public Slider micOutputSlider; 
-    public TMP_Text micOutputText; 
-    [SerializeField] EnemyAI enemyAI;
+	public float volumeThreshold = 0.1f;
+	public float sliderAmplifier = 1000f;
+	public float stalkingPositionThreshold = 0.23f;
+	public Slider micOutputSlider; 
+	public TMP_Text micOutputText; 
+	[SerializeField] EnemyAI enemyAI;
 
-    private string microphone;
-    private float micVolumeLevel;
-    private AudioClip micClip;
+	private string microphone;
+	private float micVolumeLevel;
+	private AudioClip micClip;
 
-    void Start()
-    {
-        if (Microphone.devices.Length > 0)
-        {
-            microphone = Microphone.devices[0];
-            micClip = Microphone.Start(microphone, true, 10, 44100);
-            if (micClip == null)
-            {
-                Debug.LogError("Microphone failed to start.");
-                return;
-            }
-        }
-        else
-        {
-            Debug.LogError("No microphone detected.");
-        }
+	void Start()
+	{
+		if (Microphone.devices.Length > 0)
+		{
+			microphone = Microphone.devices[0];
+			micClip = Microphone.Start(microphone, true, 10, 44100);
+			if (micClip == null)
+			{
+				Debug.LogError("Microphone failed to start.");
+				return;
+			}
+		}
+		else
+		{
+			Debug.LogError("No microphone detected.");
+		}
 
-        micOutputSlider.minValue = 0;
-        micOutputSlider.maxValue = 100; 
-        micOutputSlider.value = 0;
-        UpdateMicOutputText(0);
-    }
+		micOutputSlider.minValue = 0;
+		micOutputSlider.maxValue = 100; 
+		micOutputSlider.value = 0;
+		UpdateMicOutputText(0);
+	}
 
-    void Update()
-    {
-        micVolumeLevel = GetAveragedVolume();
-        micOutputSlider.value = micVolumeLevel * sliderAmplifier; 
-        UpdateMicOutputText(micVolumeLevel);
+	void Update()
+	{
+		micVolumeLevel = GetAveragedVolume();
+		micOutputSlider.value = micVolumeLevel * sliderAmplifier; 
+		UpdateMicOutputText(micVolumeLevel);
 
-        if (micVolumeLevel > volumeThreshold)
-        {
-            enemyAI.SetPlayerLoud(true);
-        }
-        else
-        {
-            enemyAI.SetPlayerLoud(false);
-        }
-    }
+		if (micVolumeLevel > volumeThreshold)
+		{
+			enemyAI.SetPlayerLoud(true);
+		}
+		else if (micVolumeLevel > volumeThreshold / stalkingPositionThreshold)
+		{
+			enemyAI.SetPlayerQuiet(true); 
+		}
+		else
+		{
+			enemyAI.SetPlayerLoud(false);
+		}
+	}
 
-    float GetAveragedVolume()
-    {
-        float[] data = new float[256];
-        int micPosition = Microphone.GetPosition(microphone) - 256;
-        if (micPosition < 0) return 0;
-        micClip.GetData(data, micPosition);
-        float totalVolume = 0;
-        foreach (var s in data)
-        {
-            totalVolume += Mathf.Abs(s);
-        }
-        return totalVolume / data.Length;
-    }
+	float GetAveragedVolume()
+	{
+		float[] data = new float[256];
+		int micPosition = Microphone.GetPosition(microphone) - 256;
+		if (micPosition < 0) return 0;
+		micClip.GetData(data, micPosition);
+		float totalVolume = 0;
+		foreach (var s in data)
+		{
+			totalVolume += Mathf.Abs(s);
+		}
+		return totalVolume / data.Length;
+	}
 
-    public float GetMicVolumeLevel()
-    {
-        return micVolumeLevel;
-    }
+	public float GetMicVolumeLevel()
+	{
+		return micVolumeLevel;
+	}
 
-    void UpdateMicOutputText(float value)
-    {
-        micOutputText.text = $"Mic Output Level: {value:F2}";
-    }
+	void UpdateMicOutputText(float value)
+	{
+		micOutputText.text = $"Mic Output Level: {value:F2}";
+	}
 }
